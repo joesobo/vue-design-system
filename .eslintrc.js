fe779f23c6307260f152feaeb75c996d3559f9aa
joesobo <soboleskijoe@gmail.com>
2023-02-19 14:42:49 -0800

    linting and tsconfig packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,20 @@
 module.exports = {
-	root: true,
 	env: {
 		browser: true,
 		node: true,
 	},
 	// This tells ESLint to load the config from the package `eslint-config-custom`
 	extends: ['custom'],
+	settings: {
+		tailwindcss: {
+			config: 'packages/ui/tailwind.config.js',
+			cssFiles: [
+				'**/*.css',
+				'!**/node_modules',
+				'!**/.*',
+				'!**/dist',
+				'!**/build',
+			],
+		},
+	},
 }
